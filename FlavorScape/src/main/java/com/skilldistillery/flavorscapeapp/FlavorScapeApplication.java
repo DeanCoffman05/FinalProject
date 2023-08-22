9710eec33dd0f2f70614879cf6b0945a46a9a443
Dean Coffman <deansdetail@gmail.com>
2023-08-22 18:17:49 -0400

    Create Boot Rest project, config project depend.

---
+++ FlavorScape/src/main/java/com/skilldistillery/flavorscapeapp/FlavorScapeApplication.java
@@ -0,0 +1,13 @@
+package com.skilldistillery.flavorscapeapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlavorScapeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlavorScapeApplication.class, args);
+	}
+
+}
---
+++ FlavorScape/src/test/java/com/skilldistillery/flavorscapeapp/FlavorScapeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skilldistillery.flavorscapeapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlavorScapeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
