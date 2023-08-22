1d220016fe85917ae5e21e31cbd14e1656cbf15b
Athena Hawkins <Athena.a.hawkins@gmail.coml>
2023-08-22 17:21:44 -0600

    configure hibernate, stub user entity and unit test

--- JPAFlavorScape/src/test/java/com/skilldistillery/flavorscapeapp/entities/UserTest.java
+++ JPAFlavorScape/src/test/java/com/skilldistillery/flavorscapeapp/entities/UserTest.java
@@ -1,14 +1,50 @@
 package com.skilldistillery.flavorscapeapp.entities;
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class UserTest {
+	
+	private static EntityManagerFactory emf;
+	private EntityManager em;
+	private User user;
 
-	@Test
-	void test() {
-		fail("Not yet implemented");
-	}
+	@BeforeAll
+	 static void setUpBeforeClass() throws Exception {
+	  emf = Persistence.createEntityManagerFactory("JPAFlavorScape");
+	 }
+
+	 @AfterAll
+	 static void tearDownAfterClass() throws Exception {
+	  emf.close();
+	 }
+
+	 @BeforeEach
+	 void setUp() throws Exception {
+	  em  = emf.createEntityManager();
+	  user = em.find(User.class, 1);
+	 }
+
+	 @AfterEach
+	 void tearDown() throws Exception {
+	  em.close();
+	  user = null;
+	 }
+	 
+	 @Test
+	 void test_User_entity_manager() {
+		 assertNotNull(user);
+		 assertEquals("admin", user.getUsername());
+	 }
 
 }
 
