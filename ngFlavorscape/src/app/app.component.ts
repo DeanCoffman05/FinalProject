ff3db62bf28d6113f5e328e2b075f7d004a6b52d
necron57 <jacoba1995@yahoo.com>
2023-08-23 10:07:16 -0600

    angular frontend setup and running

---
+++ ngFlavorscape/src/app/app.component.ts
@@ -0,0 +1,33 @@
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from './services/auth.service';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent implements OnInit{
+  title = 'ngFlavorscape';
+
+  constructor(
+    private auth: AuthService
+  ){}
+
+  ngOnInit() {
+    this.tempTestDeleteMeLater(); // DELETE LATER!!!
+  }
+
+  tempTestDeleteMeLater() {
+    this.auth.login('admin','test').subscribe({ // change username to match DB
+      next: (data) => {
+        console.log('Logged in:');
+        console.log(data);
+      },
+      error: (fail) => {
+        console.error('Error authenticating:')
+        console.error(fail);
+      }
+    });
+  }
+
+}
