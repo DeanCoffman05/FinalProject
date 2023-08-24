10b38b106e833b819e9e19e60e85f70ea5e3bfd8
Athena Hawkins <Athena.a.hawkins@gmail.coml>
2023-08-24 17:13:25 -0600

    review rating updated in db

--- JPAFlavorScape/src/test/java/com/skilldistillery/flavorscapeapp/entities/MenuItemReviewRatingIdTest.java
+++ JPAFlavorScape/src/test/java/com/skilldistillery/flavorscapeapp/entities/MenuItemReviewRatingIdTest.java
@@ -2,6 +2,10 @@ package com.skilldistillery.flavorscapeapp.entities;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
@@ -10,25 +14,35 @@ import org.junit.jupiter.api.Test;
 
 class MenuItemReviewRatingIdTest {
 
+	private static EntityManagerFactory emf;
+	private EntityManager em;
+	private MenuItemReviewRatingId menuItemReviewRatingid;
+
 	@BeforeAll
-	static void setUpBeforeClass() throws Exception {
-	}
+	 static void setUpBeforeClass() throws Exception {
+	  emf = Persistence.createEntityManagerFactory("JPAFlavorScape");
+	 }
 
-	@AfterAll
-	static void tearDownAfterClass() throws Exception {
-	}
+	 @AfterAll
+	 static void tearDownAfterClass() throws Exception {
+	  emf.close();
+	 }
 
-	@BeforeEach
-	void setUp() throws Exception {
-	}
+	 @BeforeEach
+	 void setUp() throws Exception {
+	  em  = emf.createEntityManager();
+	  menuItemReviewRatingid = em.find(MenuItemReviewRatingId.class, 1);
+	 }
 
-	@AfterEach
-	void tearDown() throws Exception {
-	}
-
-	@Test
-	void test() {
-		fail("Not yet implemented");
-	}
-
+	 @AfterEach
+	 void tearDown() throws Exception {
+	  em.close();
+	  menuItemReviewRatingid = null;
+	 }
+	 
+	 @Test
+	 void test_menuItemReviewRating_get_year_review_made() {
+		 assertNotNull(menuItemReviewRatingid);
+		
+	 }
 }
